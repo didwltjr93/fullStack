75f8c39e1bee3aa916c0791879170007fa36bc0f
didwltjr93 <96907252+didwltjr93@users.noreply.github.com>
2022-01-03 22:12:04 +0900

    Update Nation_Main.java

--- gwaje_file/Nation/Nation_Main.java
+++ gwaje_file/Nation/Nation_Main.java
@@ -1,11 +1,13 @@
-package gwaje_file;
+package helloWorld.src.gwaje_file.Nation;
 
 public class Nation_Main {
     public static void main(String[] args) {
         Nation_Jin_parent jinguk = new Nation_Jin_parent();
         Nation_Mahan_child mahan = new Nation_Mahan_child();
-        Nation_Jinhan_chile jinhan = new Nation_Jinhan_chile();
-        Nation_Byeonhan_chile byeonhan = new Nation_Byeonhan_chile();
+        Nation_Jinhan_child jinhan = new Nation_Jinhan_child();
+        Nation_Byeonhan_child byeonhan = new Nation_Byeonhan_child();
+
+        
         
         //진국
         System.out.println("멸망한 옛 나라의 정보는 아래와 같습니다.\n"

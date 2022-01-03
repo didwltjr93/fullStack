dcf7bfdce2a015d031ef19954976ecffd75f03fb
didwltjr93 <96907252+didwltjr93@users.noreply.github.com>
2022-01-03 16:56:54 +0900

    Update DaTe_Main.java

--- gwaje_file/day/DaTe_Main.java
+++ gwaje_file/day/DaTe_Main.java
@@ -1,1 +1,17 @@
+package gwaje_file.day;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DaTe_Main {
+    public static void main(String[] args) {
+        
+        Date dt = new Date();
+
+        SimpleDateFormat day = new SimpleDateFormat("아 오늘의 날짜는 큿흠 대략 말입니다? yyyy년 mm월 dd일 입니다만은...?\n");
+        SimpleDateFormat time = new SimpleDateFormat("시간은 대략... hh시 mm분 ss초 일듯 하군요");
+
+        System.out.println(day.format(dt)+time.format(dt));
+
+    }    
+}
